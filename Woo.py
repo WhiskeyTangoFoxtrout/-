4672934103053461e300a7d1a59daad348cc538e
WhiskeyTangoFoxtrout <120198910+WhiskeyTangoFoxtrout@users.noreply.github.com>
2024-11-04 11:50:22 -0600

    Create Woo.py

---
+++ Woo.py
@@ -0,0 +1,26 @@
+#counter?
+import curses, threading, sys, time
+from __Imax__.py import *
+
+def WooPig:#Idk if this will work
+    for i =0; i<z; i++:
+        if from++:
+            
+
+class Acounter:
+    
+    def __init__(self):
+        #ima methods sub classes(Files) here
+        pass
+
+    def mrSmith(self,mode):#Acounter.mrSmith.mode(water(True))
+    
+        def mode(self,mood,water):#set mode raw
+            self.mood = curses.raw()
+            self.water = sys.update
+
+        def headcount(self):#try to keep main files in here
+           self.runner = threading.local(threading.settrace(sys.setprofile()))
+
+            
+        
