f4a2a206fb26b6e8415235fae398993e37ee3676
WhiskeyTangoFoxtrout <120198910+WhiskeyTangoFoxtrout@users.noreply.github.com>
2024-11-04 11:35:56 -0600

    Create __Imax__.py

---
+++ __Imax__.py
@@ -0,0 +1,31 @@
+#py.3.13 might be able to make a window
+#Curses[window]
+#sys for input and output
+#pag for random mouse manip
+#this opening window for[]
+
+import curses, sys, time pyautogui as pag
+
+class mechparts:
+
+    def oraville(self):
+        self.turt = turt#one of these shells gone be a ring
+        turt = curses.doupdate,curses.def_shell_mode(),
+    
+    def Shoes(self):
+        self.sys._is_gil_enabled() = freeStand()#line for freethreaded
+        self.sys.PYTHON_JIT=0 = Compile()#I think this set the compilere
+
+    def kernWin(self,oraville):
+        self.Oraville = oraville[]
+        self.winOn = lightbulb 
+        lightblub = curses.initscr(),curses.echo(),curses.cbreak(True),kernWin.oravile.Oravile(True),
+
+
+class Imax:#remeber Imax.__init__() on comp file
+
+    def __init__(self):#navbar putbar
+        pass#the input bar is the real obj
+
+    __sysUpdate = sys.update()
+        
