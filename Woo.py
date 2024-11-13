8389641938323857306a59e0dac45717d632c2e9
WhiskeyTangoFoxtrout <120198910+WhiskeyTangoFoxtrout@users.noreply.github.com>
2024-11-13 16:16:55 -0600

    Update Woo.py

--- Woo.py
+++ Woo.py
@@ -1,26 +1,37 @@
 #counter?
 import curses, threading, sys, time
+#need a better way to add this to the threader
+
 from __Imax__.py import *
 
 def WooPig:#Idk if this will work
     for i =0; i<z; i++:
         if from++:
             
+#need to figure a priority list I'll start with time
 
-class Acounter:
+
+
+class Acounter:#maijn maain class
     
-    def __init__(self):
+    def __init__(self,Agent,gourd):
         #ima methods sub classes(Files) here
-        pass
+        self.Agent = mrSmith
+        self.gourd = []
 
-    def mrSmith(self,mode):#Acounter.mrSmith.mode(water(True))
-    
+    def mrSmith(self,mode,headcount,gord):#Acounter.mrSmith.mode(water(True)
+#threading info
+        self.gord = {}
+        
         def mode(self,mood,water):#set mode raw
             self.mood = curses.raw()
-            self.water = sys.update
+            self.water = sys.update()
+
+            if#if the mood is sent to shell?
 
         def headcount(self):#try to keep main files in here
            self.runner = threading.local(threading.settrace(sys.setprofile()))
+
 
             
         
