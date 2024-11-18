167e4f4e5f6b0348f7280d90fb938156fc6bc3ff
WhiskeyTangoFoxtrout <120198910+WhiskeyTangoFoxtrout@users.noreply.github.com>
2024-11-18 12:23:20 -0600

    Update __Imax__.py

--- __Imax__.py
+++ __Imax__.py
@@ -6,21 +6,27 @@
 
 import curses, sys, time pyautogui as pag
 
+def DaBoot(self):
+    self.Boot = Gensis()
+    pass
+    #Do I need a linker to the bootreader from here??????
+    
+    
+
 class mechparts:
 
     def oraville(self):
         self.turt = turt#one of these shells gone be a ring
-        turt = curses.doupdate,curses.def_shell_mode(),
+        turt = curses.doupdate,curses.def_shell_mode(), curses.refresh()
     
     def Shoes(self):
         self.sys._is_gil_enabled() = freeStand()#line for freethreaded
         self.sys.PYTHON_JIT=0 = Compile()#I think this set the compilere
 
-    def kernWin(self,oraville):
-        self.Oraville = oraville[]
+    def kernWin(self,oravile):
+        self.Oraville = oravile[]
         self.winOn = lightbulb 
-        lightblub = curses.initscr(),curses.echo(),curses.cbreak(True),kernWin.oravile.Oravile(True),
-
+        lightblub = curses.initscr(),curses.echo(),curses.cbreak(True),kernWin.oravile.Oravile(True),turt()
 
 class Imax:#remeber Imax.__init__() on comp file
 
