e6b8b87f97c7b5a843bb62a34b72b3b0bf6f5ff8
WhiskeyTangoFoxtrout <120198910+WhiskeyTangoFoxtrout@users.noreply.github.com>
2024-11-18 12:03:09 -0600

    Update Woo.py

--- Woo.py
+++ Woo.py
@@ -3,17 +3,13 @@ import curses, threading, sys, time
 #need a better way to add this to the threader
 
 from __Imax__.py import *
-
-def WooPig:#Idk if this will work
-    for i =0; i<z; i++:
-        if from++:
             
 #need to figure a priority list I'll start with time
-
+#just need to flush it out but this about it.
 
 
 class Acounter:#maijn maain class
-    
+    global Sheets = threading()    
     def __init__(self,Agent,gourd):
         #ima methods sub classes(Files) here
         self.Agent = mrSmith
@@ -22,16 +18,23 @@ class Acounter:#maijn maain class
     def mrSmith(self,mode,headcount,gord):#Acounter.mrSmith.mode(water(True)
 #threading info
         self.gord = {}
+        self.headcount = headcount
+        self.mode = mode
         
-        def mode(self,mood,water):#set mode raw
-            self.mood = curses.raw()
-            self.water = sys.update()
-
+        def mode(self,mood,water,lo):#set mode raw Acounter.mode.itinerary()?
+            #this just became settings. Still working on priortizing things
+            self.mood = mood(curses.raw())
+            self.water = maid(sys.update())
+            self.lo = itinerary(Sheets.active_count())
+            self.locate = locate(Sheets.current_thread())
+            
             if#if the mood is sent to shell?
 
         def headcount(self):#try to keep main files in here
            self.runner = threading.local(threading.settrace(sys.setprofile()))
+            #I havent figured a method to add other files and programs here yet
 
+        def HanLr(self):#this is whats gone be my handler to prioratize things.
+            pass
 
             
-        
