72b3b45c7c59fd91a1bae12a328be1099f83d028
WhiskeyTangoFoxtrout <120198910+WhiskeyTangoFoxtrout@users.noreply.github.com>
2024-12-03 11:49:37 -0600

    Update POE.c

--- POE.c
+++ POE.c
@@ -12,7 +12,7 @@ int main(){
         if(CONTROL_1PRIV_Msk&__get_CONTROL()){
 
             EnablePrivilegedMode();/*Make sure Priv mode is set on so the reader can upload and read*/
-        
+        }
         NVIC->[0] = 0xFFFFFFFF;/*this clears interuptions*/
         NVIC->[1] = 0xFFFFFFFF;
         NVIC->[2] = 0xFFFFFFFF;
@@ -25,12 +25,21 @@ int main(){
         SysTick -> CTRL = 0;
         SCB -> ICSR |= SCB_ICSR_PENDDSTCLR_Msk;/*This is like a try and except method*/
 
-        SCB->SHCSR &= ~( SCB_SHCSR_USGFAULTENA_Msk | \
-        SCB_SHCSR_BUSFAULTENA_Msk | \
-        SCB_SHCSR_MEMFAULTENA_Msk ) ;
-        /*I was being Lazy*/    
+        SCB->SHCSR &= ~( SCB_SHCSR_USGFAULTENA_Msk |\
+        SCB_SHCSR_BUSFAULTENA_Msk |\
+        SCB_SHCSR_MEMFAULTENA_Msk );
+        /*I was being Lazy still going to steal code for the frame*/    
+
+        if(CONTROL_SPSEL_Msk& __get_CONTROL())
+        {/*it no greenlite*/
+
+            __set_MSP(__get_PSP());
+            __set_CONTROl(__get_CONTROL()& ~CONTROL_SPSPEL_Msk);
+            
         }
         
+        SCB->VTOR = (uint32_t)Address;
+        BootJumpASM(Address[0],Address[1]);
     }
-    
+
 }
