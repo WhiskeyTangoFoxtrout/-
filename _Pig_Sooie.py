0cf7b1f4d60a1cfaa2c7b0ca2f4a7995d28b09ed
WhiskeyTangoFoxtrout <120198910+WhiskeyTangoFoxtrout@users.noreply.github.com>
2024-12-30 10:12:00 -0600

    Update _Pig_Sooie.py

--- _Pig_Sooie.py
+++ _Pig_Sooie.py
@@ -1,5 +1,5 @@
 #this is a lexer, yup and some how its gonna connect to a window and a file system
-
+#this is the skeleton of a parser had to borrow from the old acccount
 class commands:
 #remeber this is fixed
     
@@ -8,42 +8,72 @@ class commands:
         
         self.input = pac
 
-class Lexer:
-
-    def __init__(self,fn,text):
-
-        self.fn = fn
-        self.text = text
-        self.pos = Posistion(-1,0,-1)
-
-    def adv(self):
-        self.pos.adv(self.currentPos)
-        self.currentPos = self.text[self.pos.idx] if self.pos.idx < len(self.text) else None
-
 class Posistion:
 
-    def __init__(self,ln,col,idx,text):
+    def __init__(self,idx,ln,col,fn,ftxt):
 
-        self.ln = ln
-        self.col = col 
         self.idx = idx
-        self.Pm = pm
-        self.pos = {} #think i got call it commands.cursor.pos()...I think
-        
-        ln = 1
-        col = 0
+        self.ln = ln
+        self.col = col
+        self.fn = fn
+        self.ftxt  = ftxt
 
-        self.pos{ln,col}#I think this is valid
+    def adv(self, currentPos):
 
-    def adv(self,currentPos):
-        self.col +=1
-        self.idx +=1
+        self.idx += 1
+        self.col += 1
 
         if currentPos == '\n':
-            self.ln += 1
-            self.col += 0
+
+            self.ln += 1 
+            self.col = 0
 
         return self
 
     def copy(self):
-        return Posistion(self.idx,self.ln,self.col,self.idx)
+
+        return Posistion(self.idx, self.ln, self.col,self.fn, self.ftxt)
+
+#------------------------------------------- Tokens---------------------------------------------------
+
+class Tokens:
+
+    def __init__(self,type_,value=None):
+        
+        self.t
+
+    def __init__(self,fn,text):
+        
+        self.fn = fn
+        self.text = text
+        self.pos = Posistion(-1,0,-1,fn,text)
+        self.currentPos = None
+        self.adv()
+
+    def make_tokens(self):
+
+        tokens =[]
+        #wenever I figure the commands
+
+#----------------------------------------Parser-----------------------------------------------------------------
+
+class Parser:
+
+    def __init__(self,tokens):
+
+        self.tokens = tokens
+        self.tok.idx = 1
+        self.adv()
+
+    def adv(self):
+
+        self.tok_idx += 1
+        if self.tok_idx < len(self.tokens):
+            self.current_tok = self.tokens[self.tok_idx]
+        return self.current_tok
+
+#    def parse(self):
+#        res - self.expr()
+#        return res 
+
+
