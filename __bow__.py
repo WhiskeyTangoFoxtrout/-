574b5efe79e412b0cef65409c107331a4c6638e3
WhiskeyTangoFoxtrout <120198910+WhiskeyTangoFoxtrout@users.noreply.github.com>
2024-12-30 14:05:14 -0600

    Create __bow__.py

---
+++ __bow__.py
@@ -0,0 +1,23 @@
+#dis da kern gone try and make a fuction that can open a userfriendlyKernal(O.S)
+#kern kicks in where its more bit and m.c then python so when u need to look at the threadcount
+import curses
+
+class Bow:
+
+    def __init__(self,name):
+
+        self.name = title
+
+    def bootuProcess(self,window):
+
+        Window = curses.initscr()
+        curses.nobreak()
+        curses.echo()
+        curses.nocbreak()#not in the kernal
+        curses.beep('5s')
+        curses.flash()
+        curses.def_shell()
+        
+    
+    def skel(self):#I should do this in another file na hell naw
+        pass
