18db8aff4c754a36cdd075343a0fdfd7a745c15d
WhiskeyTangoFoxtrout <120198910+WhiskeyTangoFoxtrout@users.noreply.github.com>
2025-01-02 14:24:00 -0600

    Update __bow__.py

--- __bow__.py
+++ __bow__.py
@@ -1,17 +1,19 @@
 #dis da kern gone try and make a fuction that can open a userfriendlyKernal(O.S)
 #kern kicks in where its more bit and m.c then python so when u need to look at the threadcount
+#this is made for a 24inch comp moniter. Idk how to autosize it yet
 import curses
 
 class Bow:
 
-    def __init__(self,name,skel):
+    
+    def __init__(self,name,Nput):
 
-        self.skel = curses.newwin(1000,800)
+        self.nput = getstr(1915,9)
         self.name = title
         
     def bootuProcess(self,window):
 
-        Window = curses.initscr()
+        wdow = curses.initscr(1920,1000)#Wdow.newwin()
         curses.nobreak()
         curses.echo()
         curses.nocbreak()#not in the kernal
@@ -22,7 +24,9 @@ class Bow:
     
     def frame(self):
         
-        self.Frame = window.border(0,0,40,0,0,0,0,0)#think this put a line at da 40 pixels
-        self.mousePos = window.move(997,3)
-        self.runna = curses.doupdate()
+        self.Frame = wdow.border(0,0,40,0,0,0,0,0)#think this put a line at da 40 pixels
+        self.mousePos = wdow.move(1915,3)
+        self.runna = wdow.doupdate()
+        wdow.addstr(950,500,"Welcome to The O.S")
         
+
