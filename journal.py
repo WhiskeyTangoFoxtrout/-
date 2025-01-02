26012635983bbfbbfec9cc9857319f7c10e8c870
WhiskeyTangoFoxtrout <120198910+WhiskeyTangoFoxtrout@users.noreply.github.com>
2025-01-02 14:20:25 -0600

    Create journal.py

---
+++ journal.py
@@ -0,0 +1,25 @@
+#try to make this an app where you can write and save files... do I need to figure out compilers too?
+#that note was a lie this the folder system
+
+
+
+class journal:
+
+    def __init__(self):
+        pass
+
+    def Edition(self):##folders
+
+        pass 
+
+    def novel(self):#sub folder system
+
+        pass
+    
+    def pages():#actual files
+
+        if pages() == 0:
+
+            return Frame()#this a window application ima make soon
+
+        while pages() ==
